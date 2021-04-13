525dd73fafca59f269f225bd0014dd4158a375f7
kevagreen <kevareynolds@gmail.com>
2021-04-13 11:10:49 -0500

    first commit

---
+++ employeemanager/src/main/java/com/kevacodes/employeemanager/model/Employee.java
@@ -0,0 +1,11 @@
+package com.kevacodes.employeemanager.model;
+
+public class Employee {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String title;
+    private String phone;
+    private String imageUrl;
+}

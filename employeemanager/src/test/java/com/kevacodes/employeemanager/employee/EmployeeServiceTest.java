9e4f3b8881ea9eee2141691134e891245a504ed4
kevagreen <kevareynolds@gmail.com>
2021-04-19 08:51:12 -0500

    add canAddEmployee test

---
+++ employeemanager/src/test/java/com/kevacodes/employeemanager/employee/EmployeeServiceTest.java
@@ -0,0 +1,69 @@
+package com.kevacodes.employeemanager.employee;
+
+import com.kevacodes.employeemanager.model.Employee;
+import com.kevacodes.employeemanager.repository.EmployeeRepository;
+import com.kevacodes.employeemanager.service.EmployeeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeServiceTest {
+
+    @Mock
+    private EmployeeRepository employeeRepository;
+    private EmployeeService underTest;
+
+    @BeforeEach
+    void setUp(){
+        underTest = new EmployeeService(employeeRepository);
+    }
+    @AfterEach
+    void tearDown() throws Exception{
+
+    }
+    @Test
+    void canAddEmployee(){
+        //given
+        Employee testEmployee = new Employee("Testica", "test@email.com", "engineer", "3132000000","https://www.bootdey.com/img/Content/avatar/avatar7.png","23hhgg66");
+        //when
+        underTest.addEmployee(testEmployee);
+        //then
+        ArgumentCaptor<Employee> employeeArgumentCaptor = ArgumentCaptor.forClass(Employee.class);
+        verify(employeeRepository).save(employeeArgumentCaptor.capture());
+        Employee capturedEmployee = employeeArgumentCaptor.getValue();
+        assertThat(capturedEmployee).isEqualTo(testEmployee);
+    }
+    @Test
+    void canGetAllEmployees(){
+    //when
+    underTest.getAllEmployees();
+
+    //then
+    verify(employeeRepository).findAll();
+
+    }
+    @Test
+    @Disabled
+    void canUpdateEmployee(){
+
+    }
+    @Test
+    @Disabled
+    void canDeleteEmployee(){
+
+    }
+    @Test
+    @Disabled
+    void canFindEmployeeById(){
+
+    }
+}

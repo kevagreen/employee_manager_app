88f85be8515d40e733b923802f2f76970a34e35a
kevagreen <kevareynolds@gmail.com>
2021-04-19 13:01:04 -0500

    add deleteById method to EmployeeRepository

---
+++ employeemanager/src/main/java/com/kevacodes/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.kevacodes.employeemanager.repository;
+
+import com.kevacodes.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+}
